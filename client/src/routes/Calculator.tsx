37037a87ebbbe176a8763c8972da85f2bdc15a55
Andy Wu <awpats12@gmail.com>
2023-06-13 19:55:17 -0400

    Location inputs stored in Calculator component state

--- client/src/routes/Calculator.tsx
+++ client/src/routes/Calculator.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 import InputLabel from "@mui/material/InputLabel";
 import FormControl from "@mui/material/FormControl";
@@ -6,11 +6,22 @@ import TextField from "@mui/material/TextField";
 
 import ThrottledFetchButton from "../components/ThrottledFetchButton";
 
+type LocationInput = HTMLTextAreaElement | HTMLInputElement;
+
 export default function DistanceCalculator() {
+  const [pointA, setPointA] = useState('');
+  const [pointB, setPointB] = useState('');
   const [isDisabled, setDisabled] = useState(false);
 
-  function onSubmit(e: any) {
+  function onSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
+
+    console.log('submitted form!')
+  }
+
+  function onPointChange(e: React.ChangeEvent<LocationInput>, setter: React.Dispatch<React.SetStateAction<string>>) {
+    console.log(e.target.value);
+    setter(e.target.value);
   }
 
   return (
@@ -23,16 +34,24 @@ export default function DistanceCalculator() {
         <form onSubmit={onSubmit} className="distanceQueryForm">
           <div id="point-a-input">
             <InputLabel id="point-a-input-label"> Point A </InputLabel>
-            <FormControl>
-              <TextField required id="filled-basic" label="latitude,longitude" defaultValue="40.730610,-73.935242" variant="filled" />
-            </FormControl>
+            <TextField 
+              required id="filled-basic" 
+              label="latitude,longitude" 
+              defaultValue="40.730610,-73.935242" 
+              variant="filled" 
+              onChange={(event) => { onPointChange(event, setPointA) }}
+            />
           </div>
           <br/>
           <div id="point-b-input">
             <InputLabel id="point-b-input-label"> Point B </InputLabel>
-            <FormControl>
-              <TextField required id="filled-basic" label="latitude,longitude" defaultValue="36.778259,-119.417931" variant="filled" />
-            </FormControl>
+            <TextField 
+              required id="filled-basic" 
+              label="latitude,longitude" 
+              defaultValue="36.778259,-119.417931" 
+              variant="filled" 
+              onChange={(event) => { onPointChange(event, setPointB) }}
+            />
           </div>
           <br/>
           <ThrottledFetchButton type="submit" text="Submit" isDisabled={isDisabled} />
